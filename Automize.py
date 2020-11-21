03606bfbf07552f87ef8856dda2a5958f3e61c0d
Mlolor <73883601+Mlolor@users.noreply.github.com>
2020-11-21 07:25:19 +0300

    Edit Automize.py

--- Automize.py
+++ Automize.py
@@ -1,10 +1,11 @@
 import webbrowser
+import random
 
 answer = input(">>> ")
 
 if (answer == "Song"):
-	url = "https://www.youtube.com/watch?v=LE9Q4JN-Yek"
-	webbrowser.open(url, new = 0, autoraise = True)
+	url = ["https://www.youtube.com/watch?v=LE9Q4JN-Yek", "https://www.youtube.com/watch?v=iE50LyRb8eE"]
+	webbrowser.open(url[random.randint(0,len(url))], new = 0, autoraise = True)
 elif (answer == "Class Room"):
 	url = "https://classroom.google.com/u/3/h?hl=ru"
 	webbrowser.open(url, new = 0, autoraise = True)
