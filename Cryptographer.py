d8b2320d7fac457d8dcf8bc8d57a38403624297d
Mlolor <73883601+Mlolor@users.noreply.github.com>
2020-12-08 15:31:03 +0300

    Edit code

--- Cryptographer.py
+++ Cryptographer.py
@@ -2,9 +2,9 @@ import os
 import random
 import subprocess
 
-simbols = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю']
+simbols = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMёйцукенгшщзхъфывапролджэячсмитьбюЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
+letters = ""
 comands = ["Encrypt", "Decipher", "Clear", "Quit"]
-letters = []
 number = 1
 
 def Welcome():
@@ -46,10 +46,8 @@ def ReadKey():
 	global letters
 
 	with open("key.txt", "r") as f:
-		keySimbols = f.readline()
-	letters = []
-	for i in keySimbols:
-		letters.append(i)
+		letters = f.readline()
+
 	os.system("cls")
 def CreateKey():
 	UsedChar = []
